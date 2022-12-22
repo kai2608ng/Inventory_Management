3ccc195f1d159c62cfbec711badc46d5109b764d
Ng Ze Kai <113896337+kai2608ng@users.noreply.github.com>
2022-12-22 18:26:01 +0800

    Delete store directory

--- store/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import home_page, new_store_page, restock_page, sales_page
-
-urlpatterns = [
-    path('home/', home_page, name = "home_page"),
-    path('new_store/', new_store_page, name = "new_store_page"),
-    path('restock/', restock_page, name = "restock_page"),
-    path('sales/', sales_page, name = "sales_page"),
-]
